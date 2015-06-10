d61d3b6330f3fc94f3af08cd8b002ca15633fddd
sinstein <sauravkothari@Sauravs-MacBook-Pro.local>
2015-06-10 16:15:41 +0530

    Tests for corner cases

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,24 +1,28 @@
 class Rover
-	@x = 0
-	@y = 0
-	@direction = 0
+	attr_reader :x
+	attr_reader :y
+	attr_reader :direction
 	@@cardinals = ["N" , "E" , "S" , "W"]
 	
-	def initialize(x,y,direction)
-		@x = x
-		@y = y
-		@direction = direction
-		@@cardinals = ["N" , "E" , "S" , "W"]
+	def initialize(x, y, direction)
+		if is_position_valid(x, y, direction)
+			@x = x
+			@y = y
+			@direction = @@cardinals.index(direction)
+		end
+	end
+ 	
+	def raise_and_rescue(param)
+		case param
+		when 0
+ 			raise ArgumentError, "Invalid position parameters!"
+ 		when 1
+ 			raise ArgumentError, "Invalid movement command"
+ 		when 2
+ 			raise ArgumentError, "Rover will move out of bounds!"
+ 		end
 	end
 
-	def x
-		@x
-	end
-
-	def y
-		@y
-	end
-	
 	def move(x, grid)
 		case x
 		when 'R'
@@ -27,64 +31,111 @@ class Rover
 			@direction = (@direction + 3) % 4
 		when 'M'
 			translate(grid)
+		else
+			raise_and_rescue 1
 		end
-
-		return @@cardinals[@direction]
 	end
-
-
-	def is_valid(x, y, grid)
-		if x <= grid.grid_x && y <= grid.grid_y && x >= 0 && y >= 0
+ 
+ 	def is_position_valid(x, y, direction)
+ 		if (x >= 0 && y >= 0 && ["N", "E", "S", "W"].include?(direction))
+ 			return true
+ 		else
+ 			raise_and_rescue 0
+ 		end
+ 	end
+	
+	def is_move_valid(x, y, grid)
+		if x <= grid.grid_x && y <= grid.grid_y && x >= 0 && y >= 0 
 			return true
 		end
+		
+		puts "Rover will escape out of bounds on moving in direction #{@@cardinals[@direction]}!"
+		raise_and_rescue 2
 		return false
 	end
-
+ 
 	def translate(grid)
 		case @direction
 		when 0
-			if(is_valid(@x,@y + 1,grid))
+			if(is_move_valid(@x,@y + 1,grid))
 				@y += 1
 			end
 		when 1
-			if(is_valid(@x + 1,@y,grid))
+			if(is_move_valid(@x + 1,@y,grid))
 				@x += 1
 			end
 		when 2
-			if(is_valid(@x,@y - 1,grid))
+			if(is_move_valid(@x,@y - 1,grid))
 				@y -= 1
 			end
 		when 3
-			if(is_valid(@x - 1,@y,grid))
+			if(is_move_valid(@x - 1,@y,grid))
 				@x -= 1
 			end
 		else
 			puts "Invalid"
 		end
 	end
-
+ 
 	def display
 		puts "#{@x} #{@y} #{@@cardinals[@direction]}"
 	end	
 end
+ 
+
 
 class Grid
-	@grid_x = 0
-	@grid_y = 0
+	attr_accessor :grid_x
+	attr_accessor :grid_y
+ 
+ 	def raise_and_rescue
+ 		raise ArgumentError, "Invalid grid parameters!"
+ 	end
+
+ 	def is_valid(x, y)
+ 		if (x > 0 && y > 0 )
+ 			return true
+ 		end
+ 		false
+ 	end
 
 	def initialize (x, y)
-		@grid_x = x
-		@grid_y = y
-	end
+		if (!is_valid(x,y))
+			raise_and_rescue
+		else
+			@grid_y = y
+			@grid_x = x
+		end
 
-	def grid_x
-		@grid_x
-	end
-
-	def grid_y
-		@grid_y
+ 		if __FILE__ == $0
+			while true do
+				ip = gets.chomp()
+				comm = ip.split(" ")
+				rover = Rover.new(comm[0].to_i, comm[1].to_i, comm[2])
+	 
+				command = gets.chomp
+				command.each_char do |ch|
+					rover.move(ch, self)
+				end
+				rover.display
+			end
+		end
 	end
 end
+ 
 
+class MarsNavigation
+	def initialize()
+		ip = gets.chomp()
+		comm = ip.split(" ")
+		x = comm[0].to_i
+		y = comm[1].to_i
+ 
+		grid = Grid.new(x, y)
+	end
+end
+ 
 
-
+if __FILE__ == $0
+	nav = MarsNavigation.new
+end
--- test.rb
+++ test.rb
@@ -4,26 +4,45 @@ require "./mars_rover.rb"
 class TestRover < Minitest::Test
 	
 	def setup
-		@rover = Rover.new(0,0,0)
-		@grid = Grid.new(5,5)
+		@rover = Rover.new(0, 0, "N")
+		@grid = Grid.new(5, 5)
 	end
 
 	def test_grid_limits
-		assert_equal 5,@grid.grid_x
-		assert_equal 5,@grid.grid_y
+		assert_equal 5, Grid.new(5, 5).grid_x
+		assert_equal 5, @grid.grid_x
+		assert_equal 5, @grid.grid_y
+
+		assert_raises (ArgumentError) { Grid.new(-3,-3) }
+		assert_raises (ArgumentError) { Grid.new(4,-2) }
+		assert_raises (ArgumentError) { Grid.new(0,6) }
 	end
 
 	def test_direction 
-		assert_equal "W", @rover.move('L',@grid)
-		assert_equal "N", @rover.move('R',@grid)
+		@rover.move('L',@grid)
+		assert_equal 3, @rover.direction
+		
+		@rover.move('R',@grid)
+		assert_equal 0, @rover.direction
 		
 		#moving forward should not change directions
-		assert_equal "N", @rover.move('M',@grid)
-		assert_equal "E", @rover.move('R',@grid)
-		assert_equal "S", @rover.move('R',@grid)
-		assert_equal "S", @rover.move('M',@grid)
-		assert_equal "E", @rover.move('L',@grid)
-		assert_equal "N", @rover.move('L',@grid)
+		@rover.move('M',@grid)
+		assert_equal 0, @rover.direction
+
+		@rover.move('R',@grid)
+		assert_equal 1, @rover.direction
+
+		@rover.move('R',@grid)
+		assert_equal 2, @rover.direction
+
+		@rover.move('M',@grid)
+		assert_equal 2, @rover.direction
+
+		@rover.move('L',@grid)
+		assert_equal 1, @rover.direction
+
+		@rover.move('L',@grid)
+		assert_equal 0, @rover.direction
 	end
 
 	def test_translation
@@ -34,7 +53,7 @@ class TestRover < Minitest::Test
 		assert_equal 2, @rover.y
 
 		#off limit transalations
-		@rover.translate(Grid.new(2,2))
+		assert_raises (ArgumentError) {@rover.translate(Grid.new(2,2)) }
 		assert_equal 2, @rover.y
 
 		@rover.translate(@grid) 
@@ -44,6 +63,7 @@ class TestRover < Minitest::Test
 		@rover.translate(@grid)
 		assert_equal 1, @rover.x
 		#rover now faces East
+
 		@rover.translate(@grid)
 		assert_equal 2, @rover.x
 
@@ -63,10 +83,10 @@ class TestRover < Minitest::Test
 	end
 
 	def test_validity
-		assert_equal true, @rover.is_valid(4,4,@grid)
-		assert_equal false, @rover.is_valid(6,6,@grid)
-		assert_equal false, @rover.is_valid(3,6,@grid)
-		assert_equal false, @rover.is_valid(6,3,@grid)
+		assert_equal true, @rover.is_move_valid(4,4,@grid)
+		assert_raises (ArgumentError) { @rover.is_move_valid(6,6,@grid)}
+		assert_raises (ArgumentError) { @rover.is_move_valid(3,6,@grid)}
+		assert_raises (ArgumentError) { @rover.is_move_valid(6,3,@grid)}
 	end
 
 end
